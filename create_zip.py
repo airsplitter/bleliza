21b3ad01b3ef651750bf8f5688af52a84963b76f
Christoph <c.maschowski@lotus-vita.de>
2026-01-24 14:23:38 +0100

    refactor: rename texturetools module to utilities

--- create_zip.py
+++ create_zip.py
@@ -1,8 +1,16 @@
 import shutil
 import os
 
+zip_name = 'bleliza_utilities'
+dir_name = 'bleliza_utilities'
+
+# Remove old zip if it exists
 if os.path.exists('bleliza_texturetools.zip'):
     os.remove('bleliza_texturetools.zip')
+    print("Old zip removed.")
 
-shutil.make_archive('bleliza_texturetools', 'zip', '.', 'bleliza_texturetools')
-print("Zip file created successfully.")
+if os.path.exists(f'{zip_name}.zip'):
+    os.remove(f'{zip_name}.zip')
+
+shutil.make_archive(zip_name, 'zip', '.', dir_name)
+print(f"{zip_name}.zip created successfully.")
